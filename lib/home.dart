aa395f7d03d832edc22cfc3b0b1374b4ad07889e
FlutLab.io <>
2022-04-09 07:50:57 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -104,3 +104,250 @@ Widget _drawerItem({IconData? icon, required String text, GestureTapCallback? on
     onTap: onTap,
   );
 }
+
+class Contact extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contact"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text("Contact", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(filled: true, hintText: "Name", fillColor: Colors.white, border: InputBorder.none),
+                  ),
+                  SizedBox(height: 8),
+                  TextField(
+                    decoration: InputDecoration(filled: true, hintText: "Email", fillColor: Colors.white, border: InputBorder.none),
+                  ),
+                  SizedBox(height: 8),
+                  TextField(
+                    decoration: InputDecoration(filled: true, hintText: "Phone", fillColor: Colors.white, border: InputBorder.none),
+                  ),
+                  SizedBox(height: 8),
+                  MaterialButton(
+                    height: 60,
+                    minWidth: double.infinity,
+                    color: Color(0xff333333),
+                    onPressed: () {},
+                    child: Text("SUBMIT",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        )),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Galery extends StatelessWidget {
+  final List<String> foto = [
+    "asset/image/rj.jpeg",
+    "asset/image/jn.jpeg",
+    "asset/image/hc.jpeg",
+    "asset/image/jm.jpeg",
+    "asset/image/ch.jpeg",
+    "asset/image/js.jpeg",
+    "asset/image/mk.jpeg"
+  ];
+
+  final List<String> sub = [
+    'NCT Dream Renjun dikenal sebagai salah satu member NCT yang paling estetik',
+    'NCT Dream Jeno merupakan salah satu member NCT yang kehadirannya sudah dikenal sejak ia masih remaja.',
+    'NCT Dream Haechan diketahui sebagai salah satu idol yang memiliki otak yang cerdas dan kreatif',
+    'NCT Dream Jaemin Paras Jaemin yang menawan ini selalu bisa mencuri perhatian para NCTzen',
+    'NCT Dream Chonle dikenal sebagai Chenle, adalah seorang penyanyi, penulis lagu, penari dan pemeran Tiongkok yang berbasis di Seoul, Korea Selatan.',
+    'NCT Dream Jisung seorang penyanyi Korea Selatan yang berada di bawah naungan SM Entertainment.',
+    'NCT Dream Mark Mark Lee, lebih dikenal dengan nama Mark adalah rapper, dancer, dan penyanyi berkebangsaan Kanada'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Galery"),
+      ),
+      body: GridView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 30,
+        ),
+        itemCount: foto.length,
+        itemBuilder: (context, index) {
+          return Container(
+            child: ListTile(
+              title: ClipRRect(
+                  child: Image.asset(
+                foto[index],
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+              )),
+              subtitle: Text(sub[index], style: TextStyle(color: Colors.black)),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class musik extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Padding(padding: new EdgeInsets.all(20.0)),
+            new Text(
+              "Musik",
+              style: new TextStyle(fontSize: 30.0),
+            ),
+            new Padding(padding: new EdgeInsets.all(20.0)),
+            new Icon(
+              Icons.audiotrack,
+              size: 90.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Portofolio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Portofolio"),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: <Widget>[
+          new Image.asset(
+            'asset/image/bunga1.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'asset/image/bunga2.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'asset/image/bunga3.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'asset/image/bunga4.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'asset/image/bunga5.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'asset/image/bunga6.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'asset/image/bunga7.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          new Image.asset(
+            'asset/image/bunga8.jpg',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Profil extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Profil"),
+        ),
+        body: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 0)),
+            new Image.asset(
+              'asset/image/jejes.jpg',
+              width: 250,
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+            Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 15)),
+            Center(
+              child: Text(
+                "Nama               : Sekar Ananta Putri Jesica\n"
+                "Nim                  : 362055401022\n"
+                "Program Studi  : Teknik Informatika\n"
+                "Jenis Kelamin    : Perempuan\n",
+                overflow: TextOverflow.clip,
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class Setting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Setting'),
+      ),
+      body: Column(children: [
+        Center(
+          child: Text(
+            "INFORMATION\n\n,"
+            "LOREM\n"
+            "(Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id iaculis sapien, ac bibendum dolor. Suspendisse rutrum mauris est, imperdiet congue dui suscipit nec. Suspendisse potenti. Vestibulum consectetur faucibus lacus, volutpat rhoncus neque dapibus a. Nunc consectetur nibh dui, sed semper massa consequat non. Cras lobortis molestie velit, sit amet aliquet magna accumsan a. Aliquam mollis eros at nisl luctus interdum et luctus sapien. Aliquam sed vestibulum nulla, eu porttitor ante. Aliquam turpis tellus, vestibulum ut posuere id, cursus eget neque. Etiam malesuada ligula odio, nec egestas leo bibendum vel. Phasellus iaculis nisi nulla, id tempor dolor porttitor vulputate. Donec in mauris porttitor, congue ipsum vitae, porta velit.)\n",
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- lib/profil.dart
+++ lib/profil.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
 
-class Profil extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Profil"),
-        ),
-        body: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 0)),
-            new Image.asset(
-              'asset/image/jejes.jpg',
-              width: 250,
-              height: 250,
-              fit: BoxFit.cover,
-            ),
-            Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 15)),
-            Center(
-              child: Text(
-                "Nama               : Sekar Ananta Putri Jesica\n"
-                "Nim                  : 362055401022\n"
-                "Program Studi  : Teknik Informatika\n"
-                "Jenis Kelamin    : Perempuan\n",
-                overflow: TextOverflow.clip,
-                style: TextStyle(fontSize: 18, color: Colors.black),
-                textAlign: TextAlign.start,
-              ),
-            ),
-          ],
-        ));
-  }
-}
+// class Profil extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text("Profil"),
+//         ),
+//         body: Column(
+//           //mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 0)),
+//             new Image.asset(
+//               'asset/image/jejes.jpg',
+//               width: 250,
+//               height: 250,
+//               fit: BoxFit.cover,
+//             ),
+//             Padding(padding: EdgeInsets.fromLTRB(0, 15, 0, 15)),
+//             Center(
+//               child: Text(
+//                 "Nama               : Sekar Ananta Putri Jesica\n"
+//                 "Nim                  : 362055401022\n"
+//                 "Program Studi  : Teknik Informatika\n"
+//                 "Jenis Kelamin    : Perempuan\n",
+//                 overflow: TextOverflow.clip,
+//                 style: TextStyle(fontSize: 18, color: Colors.black),
+//                 textAlign: TextAlign.start,
+//               ),
+//             ),
+//           ],
+//         ));
+//   }
+// }
