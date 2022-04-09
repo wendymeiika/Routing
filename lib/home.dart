4e28a3de6d658d0fec9f54a786b804239673878c
FlutLab.io <>
2022-04-09 16:53:21 +0000

    Autosave

--- lib/home.dart
+++ lib/home.dart
@@ -332,15 +332,21 @@ class Setting extends StatelessWidget {
         title: Text('Setting'),
         backgroundColor: Colors.pink[800],
       ),
-      body: Column(children: [
-        Center(
-          child: Text(
-            "INFORMATION\n\n,"
-            "LOREM\n"
-            "(Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id iaculis sapien, ac bibendum dolor. Suspendisse rutrum mauris est, imperdiet congue dui suscipit nec. Suspendisse potenti. Vestibulum consectetur faucibus lacus, volutpat rhoncus neque dapibus a. Nunc consectetur nibh dui, sed semper massa consequat non. Cras lobortis molestie velit, sit amet aliquet magna accumsan a. Aliquam mollis eros at nisl luctus interdum et luctus sapien. Aliquam sed vestibulum nulla, eu porttitor ante. Aliquam turpis tellus, vestibulum ut posuere id, cursus eget neque. Etiam malesuada ligula odio, nec egestas leo bibendum vel. Phasellus iaculis nisi nulla, id tempor dolor porttitor vulputate. Donec in mauris porttitor, congue ipsum vitae, porta velit.)\n",
-          ),
-        ),
-      ]),
+      body: new Container(
+          padding: EdgeInsets.all(20),
+          child: ListView(
+            children: [
+              Center(
+                child: Text("Information", style: TextStyle(color: Colors.black38, fontWeight: FontWeight.bold, fontSize: 25)),
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              Text("     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+              Padding(padding: EdgeInsets.all(10)),
+              Text("     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."),
+              Padding(padding: EdgeInsets.all(10)),
+              Text("     There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.")
+            ],
+          )),
     );
   }
 }
